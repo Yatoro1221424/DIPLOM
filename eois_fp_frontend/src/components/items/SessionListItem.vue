88c57550c2a9feaf76815ee7d71d5d3abdc085c9
Sergeu <sergei.boyakarev@yandex.ru>
2022-06-17 11:52:24 +0700

    4

--- eois_fp_frontend/src/components/items/SessionListItem.vue
+++ eois_fp_frontend/src/components/items/SessionListItem.vue
@@ -1,90 +1,46 @@
 <template>
   <v-expansion-panels focusable>
-    <v-expansion-panel v-for="(item,i) in 5"
-      :key="i">
+    <v-expansion-panel> 
       <v-expansion-panel-header expand-icon="mdi-menu-down">
-        <h3>Место проведение: Кампус</h3>
+        <h3>Место проведение: {{sessions.place}}</h3>
         <v-spacer/>
-        <p>Дата начала </p>
-        <p>Дата конца </p>
-        <p>Статус</p>
+        <p>Дата начала: {{sessions.dateStart}} </p>
+        <p>Дата конца: {{sessions.dateEnd}}</p>
       </v-expansion-panel-header>
       <v-expansion-panel-content>
-        <v-list-item-content>
-          <v-rov>
-            <v-col>
-              <v-menu
-                ref="menu"
-                v-model="menu"
-                :close-on-content-click="false"
-                :return-value.sync="date"
-                transition="scale-transition"
-                offset-y 
-               
-                min-width="auto">
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field
-                      v-model="dateRangeText"
-                      label="Дата начала и окончания"
-                      prepend-icon="mdi-calendar"
-                      readonly 
-                      disabled
-                      v-bind="attrs"
-                      v-on="on"
-                  ></v-text-field>
-                </template>
-                <v-date-picker :landscape="true" color="blue-grey darken-2" locale="ru"  range > 
-                    <v-spacer></v-spacer>
-                    <v-btn text color="primary" @click="menu = false">
-                      Отколнить
-                    </v-btn>
-                    <v-btn text color="primary" @click="$refs.menu.save(date)">
-                      Принять
-                    </v-btn>
-                </v-date-picker>
-              </v-menu>
-              <v-text-field
-                label="Место проведение"
-                prepend-icon="mdi-bank-plus"
-                disabled
-                required
-              ></v-text-field>
-              <v-spacer/>
-              <v-textarea
-                outlined
-                disabled
-                name="description"
-                label="Описание"
-                prepend-icon="mdi-order-alphabetical-descending"
-              ></v-textarea>
-              <v-combobox
-                label="Проекты"
-                disabled
-                prepend-icon="mdi-clipboard-list-outline"
-                clearable
-                multiple
-                outlined
-                small-chips
-            ></v-combobox>
-            </v-col>
-          </v-rov>
-        </v-list-item-content>
+              <v-list-item-content>
+                <v-col>
+                <v-text-field
+                    label="Краткое описание"
+                    v-model="sessions.description"
+                    prepend-icon="mdi-order-alphabetical-descending"
+                    outlined
+                    readonly
+                    required
+                ></v-text-field>
+                <v-spacer/>
+                <v-textarea
+                    label="Описание"
+                    v-model="sessions.fullDescription"
+                    outlined
+                    readonly
+                    prepend-icon="mdi-order-alphabetical-descending"
+                ></v-textarea>
+                <v-btn color="primary" @click="onClick">Подать заявку на сессию</v-btn>
+                </v-col>
+              </v-list-item-content>
       </v-expansion-panel-content>
     </v-expansion-panel>
   </v-expansion-panels>
 </template>
 
 <script>
+import { mapActions, } from 'vuex'
 export default {
-    data: () => ({
-        dates: ['2019-09-10', '2019-09-20'],
-        date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
-        menu: false,
-    }),
-    computed: {
-      dateRangeText () {
-        return this.dates.join(' ~ ')
-      },
-    },
+  name: "SessionsEntryListItem",
+  props: ['sessions'],
+  methods: {
+      ...mapActions('admin', ['fetchSessions', 'sendSession'])
+  }
 }
 </script>
--- eois_fp_frontend/src/components/lists/SessionList.vue
+++ eois_fp_frontend/src/components/lists/SessionList.vue
@@ -1,12 +1,24 @@
 <template>
-    <SessionListItem />
+    <div>
+        <SessionListItem v-for="sessions in getSessions" :key="sessions.id" :session="sessions"/>
+    </div>
 </template>
 <script>
 import SessionListItem from '../items/SessionListItem.vue'
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
     components: {
         SessionListItem
+    },
+    computed: {
+        ...mapGetters('admin', ['getSessions'])
+    },
+    methods: {
+        ...mapActions('admin', ['fetchSessions'])
+    },
+    async mounted() {
+        await this.fetchSessions() 
     }
 }
 </script>
